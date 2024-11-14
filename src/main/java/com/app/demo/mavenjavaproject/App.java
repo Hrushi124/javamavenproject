3a1af615db1359b17fd6e6a10e2a6daed3695eff
Hrushi124 <hrushipothireddy@gmail.com>
2024-11-14 15:16:48 +0530

    added maven project

---
+++ src/main/java/com/app/demo/mavenjavaproject/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.mavenjavaproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello HRushi 22bd1a050g");
+    }
+}
